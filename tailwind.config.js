339b1beae8da46a0edcf97f84646e1ff9948f7cc
Katriin Liselle Eenmaa <kleenmaa@tlu.ee>
2024-03-19 13:33:41 +0200

    Logode slider ja sektsioon 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,20 @@ module.exports = {
     fontSize: {
       sm: '0.9375rem',
       base: '1.1rem',
+      'xl': '1.4rem',
       '5xl': ['4rem', 1.05],
       '2xl': ['1.5rem', 1.05],
     },
     extend: {
+      animation: {
+        "infinite-scroll": "infinite-scroll 80s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
       fontFamily: {
         body:['Roboto'],
         title:['Chonburi'],
